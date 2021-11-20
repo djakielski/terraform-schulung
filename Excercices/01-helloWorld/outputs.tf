db05dbc272788e118700fef5c53d13882259bd9a
schmalstieg <stefan.schmalstieg@adesso.de>
2021-11-20 13:20:26 +0100

    Added simple example - hello world webserver

---
+++ Excercices/01-helloWorld/outputs.tf
@@ -0,0 +1,8 @@
+output "public_ip" {
+  value       = aws_instance.webserver.public_ip
+  description = "public IP address of our webserver"
+}
+output "dns_name" {
+  value       = aws_instance.webserver.public_dns
+  description = "public dns name of our webserver"
+}
