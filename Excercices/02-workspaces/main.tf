5e7e20252be77874e650adcc80c1ef2809cd5544
schmalstieg <stefan.schmalstieg@adesso.de>
2021-11-20 17:02:04 +0100

    added adaption of simple hello world webserver to support multiple workspaces

---
+++ Excercices/02-workspaces/main.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region="us-east-1"
+}
+
+provider "aws" {
+  region="eu-central-1"
+  alias = "eu"
+}
+
+resource "aws_instance" "webserver"{
+  ami = data.aws_ami.latest_hvm_ubuntu.id
+  instance_type = var.instance_type
+  security_groups = [aws_security_group.webserver.name]
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "Hello, World" > index.html
+    nohup busybox httpd -f -p 80 &
+    EOF
+  tags = {
+    Name = "webserver - ${terraform.workspace}"
+    Project = "TF_Training"
+    LastModifiedAt = formatdate("EEEE, DD.MM.YYYY hh:mm:ss ZZZ",timestamp())
+  }
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+resource "aws_security_group" "webserver"{
+  name="webserver-${random_pet.postfix.id}"
+  ingress {
+    from_port = 8080
+    protocol = "tcp"
+    to_port = 8080
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "random_pet" "postfix" {
+  keepers = {
+    # Generate a new pet name each time we switch to a new AMI id
+    ami_id = data.aws_ami.latest_hvm_ubuntu.id
+  }
+}
+
+data "aws_ami" "latest_hvm_ubuntu" {
+  most_recent = true
+  owners = ["099720109477"] # Canonical
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm/ubuntu-*-*-amd64-server-*"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
---
+++ Excercices/02-workspaces/variables.tf
@@ -0,0 +1,5 @@
+variable "instance_type" {
+  type        = string
+  default     = "t3.nano"
+  description = "Type of the EC2 Instance for TF Training."
+}
